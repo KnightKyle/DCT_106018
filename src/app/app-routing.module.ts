862269d7c2e0793ae11d5cdd3b46821ec826785a
Will保哥 <doggy.huang@gmail.com>
2017-04-26 16:49:23 +0800

    13 請加入 /login 頁面，並將原有頁面都放入 Layout 元件下

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,30 @@
+import { LayoutComponent } from './layout/layout.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, Route } from '@angular/router';
+import { Routes, RouterModule, Route, PreloadAllModules } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'cards/:type', component: CardsComponent },
-  { path: 'charts',
-    loadChildren: './charts/charts.module#ChartsModule'
-  }
+  { path: '', component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'cards/:type', component: CardsComponent },
+      { path: 'charts',
+        loadChildren: './charts/charts.module#ChartsModule'
+      }
+    ]
+  },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     useHash: false,
-    enableTracing: false
+    enableTracing: false,
+    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
 })
