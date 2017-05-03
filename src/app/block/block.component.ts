da3f31d4017ae471bd42655196294978645ec607
Will保哥 <doggy.huang@gmail.com>
2017-05-03 16:27:32 +0800

    示範 @ContentChild 與 <ng-content> 的用法

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-block',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockComponent implements OnInit {
 
+  @ContentChild('btn')
+  btn: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    this.btn.nativeElement.innerHTML = '100';
   }
 
 }
