1e2f050848dc1a78cbbd4907c49c5e151cef1242
Will保哥 <doggy.huang@gmail.com>
2017-05-03 13:50:17 +0800

    18 練習第一個 Reactive Forms 開發方式

--- src/app/forms/classic2/classic2.component.ts
+++ src/app/forms/classic2/classic2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-classic2',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Classic2Component implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
+  data: any = {
+    'name': 'Will',
+    'sid': 'A123456789'
+  };
+  form: FormGroup;
   ngOnInit() {
+    this.form = this.fb.group({
+      'name': 'Will',
+      'sid': 'A123456789'
+    });
   }
 
 }
